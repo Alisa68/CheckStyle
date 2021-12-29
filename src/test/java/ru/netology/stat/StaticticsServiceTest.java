04baf33c907d3e1790b284e197bcdb39b84b38b1
Alisa68 <lebedeva19682@yandex.ru>
2021-12-29 18:19:10 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stat/StaticticsServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.stat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StaticticsServiceTest {
+
+    @Test
+    void findMax() {
+        StaticticsService service = new StaticticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+    }
+}
